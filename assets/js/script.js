bc7f02deac157c2415f22ccf8b0b6d68ae0e77b1
georgielill <georgielills@gmail.com>
2024-01-08 22:50:23 +0000

    added js to display current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,27 @@ $(document).ready(function () {
 
   setInterval(displayTime, 1000)
 })
+function updateHour() {
+  let currentHour = dayjs().hour()
+  let timeBlocks = $('.time-block')
+
+  timeBlocks.each(function () {
+    let blockHour = parseInt($(this).attr('id'));
+
+
+    if (currentHour > blockHour) {
+      $(this).removeClass("present future");
+      $(this).addClass("past");
+    } else if (currentHour === blockHour) {
+      $(this).removeClass("past future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past present");
+      $(this).addClass("future");
+    }
+
+  });
+}
+setInterval(updateHour, 15000);
+
+
