10978b638d512fdd4395ec7ad05bde3196c916c8
georgielill <georgielills@gmail.com>
2024-01-08 23:51:01 +0000

    added functionality for colours past present future depending on current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,28 +7,29 @@ $(document).ready(function () {
   }
 
   setInterval(displayTime, 1000)
+
+  function updateHour() {
+    let currentHour = dayjs().hour()
+    console.log(currentHour);
+    let timeBlocks = $('.time-block')
+    console.log(timeBlocks)
+
+    timeBlocks.each(function () {
+      let blockHour = parseInt($(this).attr('id'))
+      console.log(blockHour);
+
+      if (currentHour > blockHour) {
+        $(this).addClass('past')
+      } else if (currentHour === blockHour) {
+        $(this).removeClass('past')
+        $(this).addClass('present')
+      } else
+        $(this).removeClass('past')
+      $(this).removeClass('present')
+      $(this).addClass('future')
+    });
+  }
+  setInterval(updateHour, 15000)
 })
-function updateHour() {
-  let currentHour = dayjs().hour()
-  let timeBlocks = $('.time-block')
 
-  timeBlocks.each(function () {
-    let blockHour = parseInt($(this).attr('id'));
-
-
-    if (currentHour > blockHour) {
-      $(this).removeClass("present future");
-      $(this).addClass("past");
-    } else if (currentHour === blockHour) {
-      $(this).removeClass("past future");
-      $(this).addClass("present");
-    } else {
-      $(this).removeClass("past present");
-      $(this).addClass("future");
-    }
-
-  });
-}
-setInterval(updateHour, 15000);
-
-
+// Add save button functionality
