11e1de1ec7e232777334ee192022eb14371d61dd
georgielill <georgielills@gmail.com>
2024-01-09 21:28:10 +0000

    added save functionality in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,53 @@
 $(document).ready(function () {
 
   function displayTime() {
-    $('#currentDay').text(dayjs().format("dddd, MMMM D, YYYY"))
-    $('#currentTime').text(dayjs().format("h:mm:ss"))
-
+    $(`#currentDay`).text(dayjs().format(`dddd, MMMM D, YYYY`))
+    $(`#currentTime`).text(dayjs().format(`HH:mm A`))
   }
+
 
   setInterval(displayTime, 1000)
 
   function updateHour() {
-    let currentHour = dayjs().hour()
-    console.log(currentHour);
-    let timeBlocks = $('.time-block')
-    console.log(timeBlocks)
+    let currentHour = dayjs().hour();
+    let timeBlocks = $(".time-block");
 
     timeBlocks.each(function () {
-      let blockHour = parseInt($(this).attr('id'))
-      console.log(blockHour);
+      let blockHour = parseInt($(this).attr("id"));
 
       if (currentHour > blockHour) {
-        $(this).addClass('past')
+        $(this).removeClass("present future");
+        $(this).addClass("past");
       } else if (currentHour === blockHour) {
-        $(this).removeClass('past')
-        $(this).addClass('present')
-      } else
-        $(this).removeClass('past')
-      $(this).removeClass('present')
-      $(this).addClass('future')
+        $(this).removeClass("past future");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past present");
+        $(this).addClass("future");
+      }
     });
   }
-  setInterval(updateHour, 15000)
-})
 
-// Add save button functionality
+  // Calling the function to update the hours
+  updateHour();
+
+  $('.saveBtn').on('click', function () {
+    let textValue = $(this).siblings('.description').val();
+    let timeValue = $(this).parent().attr('id');
+    localStorage.setItem(timeValue, textValue);
+    console.log(`Saved: ${timeValue} - ${textValue}`);
+  });
+
+  $('.time-block').each(function () {
+    let hour = $(this).attr('id');
+    let storedValue = localStorage.getItem(hour);
+    $(this).find('.description').val(storedValue);
+    console.log(`Retrieved: ${hour} - ${storedValue}`);
+  });
+
+  $(`#clearAllBtn`).on(`click`, function () {
+    localStorage.clear()
+    document.location.reload();
+  })
+
+});
